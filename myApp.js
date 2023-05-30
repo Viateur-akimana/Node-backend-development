c04e8bcc4bb5452a5adfab023199e339de324475
akimana-viateur <akimanaviateur94@gmail.com>
2023-05-30 15:48:57 +0200

    challenges

---
+++ myApp.js
@@ -0,0 +1,42 @@
+let express = require('express');
+let app = express();
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
